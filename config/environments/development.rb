3b7c1d9e0f9b70d103bae028bfff4a2f042de429
Necati Erol <necati.erol@wext.com>
2021-05-08 15:53:28 +0300

    mailler

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
+
+  # Default Mailer Host
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # mailcatcher
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
